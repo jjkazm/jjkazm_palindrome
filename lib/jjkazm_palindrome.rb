383197b759b41a44d6b94d4cefa348b172c07a8f
jjkazm <jjkazm@gmail.com>
2019-02-04 15:16:58 +0100

    Finish working and refactored palindrome method

--- lib/jjkazm_palindrome.rb
+++ lib/jjkazm_palindrome.rb
@@ -1,6 +1,17 @@
 require "jjkazm_palindrome/version"
 
-module JjkazmPalindrome
-  class Error < StandardError; end
-  # Your code goes here...
+class String
+
+  # Returns true for a palindrome, false otherwise.
+  def palindrome?
+    processed_content == processed_content.reverse
+  end
+
+  private
+
+    # Returns content for palindrome testing.
+    def processed_content
+      self.downcase.gsub(/\W/, "")
+    end
+
 end
